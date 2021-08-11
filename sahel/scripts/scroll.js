4c7edbc186b4847982f80a1e032b34eb0294ca77
abhishek-deshmukh <deshmukh.abhishek369@gmail.com>
2021-08-11 12:24:05 +0530

    changes from sahel pushed

--- sahel/scripts/scroll.js
+++ sahel/scripts/scroll.js
@@ -7,8 +7,16 @@ function fadeInOnScroll() {
     const elementTop = element.getBoundingClientRect().top;
     const elementBottom = element.getBoundingClientRect().bottom;
 
-    if (elementTop < windowHeight - bufferHeight) {
+    if (
+      elementTop < windowHeight - bufferHeight &&
+      elementBottom > bufferHeight
+    ) {
       element.classList.add("on-scroll-active");
+    }
+    if (innerWidth < 768) {
+      if (elementBottom < 0 || elementTop > windowHeight) {
+        element.classList.remove("on-scroll-active");
+      }
     }
   });
 }
